c20203aa353e6be1a79ae04c63f919183f9f42c5
ttkn <tonytran55@gmail.com>
2017-01-02 21:39:50 -0800

    new method adopted
    - used a list of dates to subset data for week/month transactions
    - removed loops for dca calculation
    - functions now take annual amt as args instead of per transaction amts

--- ira methods.py
+++ ira methods.py
@@ -9,8 +9,6 @@ Which purchasing method will yield more shares, the lump sum or dca?
 '''
 
 import pandas as pd
-import numpy as np
-from datetime import datetime, date, time
 
 df = pd.read_csv(r'c:\resolve\projects\IRAs\v2055.csv') #index_col=['y','m','w'],skipinitialspace=True)
 
@@ -20,9 +18,10 @@ df.Date = pd.to_datetime(df.Date)
 df.index = df.Date
 df = df.sort_index(ascending=True)
 
+
 def dca(amount, interval='week'):
     '''
-    amount = how much to spend at a time
+    amount = annual contribution
     interval = how often
     Returns total number of shares purchased
     
@@ -31,8 +30,7 @@ def dca(amount, interval='week'):
     - if using multi index, cannot set the index as date and do groupby year/month/etc
     - if using resample, also can't group by y/m/etc
     
-    '''
-    '''solved issues:
+    solved issues:
     ===================
     [basic draft OK. still need to add something for interval of purchase]
     - [possible raw input options: week, month]
@@ -40,46 +38,29 @@ def dca(amount, interval='week'):
     - [can't print a statement plus variables] SOLVED: used str(x)
     - [grouped data frame doesn't work with iterrows] SOLVED: added loop layer
     - [add storage for shares purchased per year]  
-    '''
-    shares = 0
-    shares_table = {}
-    prices = []
+    
+    failed grouping methods:
+    ========================
     #df_week = df.groupby()
     #df_month = df.groupby(['y', pd.Grouper(freq='m')]).first   # 'method' object is not iterable
     #df_month = df.resample('m').resample('a')
-    df_month = df.resample('m').first()
+    #df_month = df.resample('m').first()'''
+  
     if interval == 'week':
-        df1 = df_week
+        i_week = pd.date_range('2012/1/1',periods=60,freq='7D') #generates date range by week
+        reduca = df.index.searchsorted(i_week)  # .serachsorted helps align 2 arrays
+        amount = amount/52
     elif interval == 'month':
-        df1 = pd.DataFrame(df_month).groupby('y')
+        i_month = pd.date_range('2012/1/1',periods=60,freq='30D') #generates date range by month
+        reduca = df.index.searchsorted(i_month)
+        amount = amount/12
     else:
         print('Interval must be week or month')
-    
-    def buy(x):
-        shares_bought = amount/x
-        return shares_bought
-    def track(x):
-        limit += amount
-        return limit
-    df['shares'] = df.Close.apply(buy)
-    
-    for eh, x in df1:    # i=year, x=dataframe
-        limit = 0       # limit resets every year
-    
-        for e, row in x.iterrows(): # for each row in 2012+
-            if limit < 5500:
-                shares_bought = amount/row.Close    #basic transactional details
-                limit += amount
-                shares += shares_bought
-                print('{0} - {1} shares. ${2} spent'.format(row.Date, shares, limit))
-                
-                #else: # for each year, contribution limit is 5500
-                    #amount_adj = limit - 5500
-                    #shares += (amount + amount_adj)/row.Close
-                    #print('{0} - {1} shares. ${2} spent'.format(e.year, shares, limit))
-                    
-                
-        return shares
+        
+    df['shares'] = amount/df.Close[reduca]  # subsets df and gets # of shares purchased
+    ok = df.dropna()
+    result = ok.groupby('y').sum()
+    return result
 
 def lump(amount):
     df2 = df.groupby(df.y).last() # .last() returns the last line of each group
